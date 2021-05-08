58f5dd2333677cbf5733108c6a6cbc517812e5f6
AaronR <beingbetter21@gmail.com>
2021-05-08 09:19:33 +0530

    done

--- block-BNaaau/server.js
+++ block-BNaaau/server.js
@@ -1,97 +1,123 @@
+/*
+
+Q. Handle 2 requests on same route with different method
+    1. GET on '/users' route where return a simple HTML form with name and email field
+    2. POST on '/users' route with a message 'Posted for the second time'.
+
+Q. create a server and handle query params from the request on following url i.e. `/users?email=nodeserver@gmail.com` from browse
+  - parse the  request url using parse method from url module
+  - console pathname from parsed url in above step
+  - grab url using `req.url`
+  - differentiate between `req.url` and `parsedUrl.pathname`
+  - grab the email from query params
+  - return json response with email from query params */
+
 let http = require('http');
 let fs = require('fs');
 
-let serverOne = http.createServer(handleServerOne);
-let serverTwo = http.createServer(handleServerTwo);
-let serverThree = http.createServer(handleServerThree);
-let serverFour = http.createServer(handleServerFour);
-let serverFive = http.createServer(handleServerFive);
-let serverSix = http.createServer(handleServerSix);
-let serverSeven = http.createServer(handleServerSeven);
-let serverEight = http.createServer(handleServerEight);
-let serverNine = http.createServer(handleServerNine);
-
-function handleServerOne(req, res) {
+server1=http.createServer(serverOne)
+server1.listen(5000,()=>{console.log("Port:5000");})
+function serverOne(req,res){
   console.log('This is request object');
   console.log(req);
   console.log('This is response object');
   console.log(res);
 }
 
-function handleServerTwo(req, res) {
-  res.end('My First server in NodeJS');
+server2=http.createServer(serverTwo);
+server2.listen(5100,()=>{console.log("Port:5100");})
+function serverTwo(req,res){
+    res.end('My First server in NodeJS');
 }
 
-function handleServerThree(req, res) {
-  console.log(req.headers);
-  console.log(req.headers['user-agent']);
+
+server3=http.createServer(serverThree)
+server3.listen(5555,()=>{console.log("Port:5555");})
+function serverThree(req,res){
+    console.log(req.headers);
+    console.log(req.headers['user-agent']);
 }
 
-function handleServerFour(req, res) {
+
+server4=http.createServer(serverFour)
+server4.listen(5566,()=>{console.log("Port:5566");})
+function serverFour(req,res){
   console.log(req.url, req.method);
   res.end(req.url, req.method);
 }
 
-function handleServerFive(req, res) {
-  //   res.end(req.header);
+
+server5=http.createServer(serverFive)
+server5.listen(7000,()=>{console.log("Port:7000");})
+function serverFive(req,res){
+    res.end(req.headers);
 }
 
-function handleServerSix(req, res) {
-  res.statusCode = 202;
-  res.end('From server 6');
+
+server6=http.createServer(serverSix)
+server6.listen(3333,()=>{console.log("Port:3333");})
+function serverSix(req,res){
+    res.statusCode = 202;
+    res.end();
 }
 
-function handleServerSeven(req, res) {
-  res.setHeader('Content-type', 'text/html');
-  res.end(`<h3>Welcome to Altcampus</h3>`);
+
+server7=http.createServer(serverSeven)
+server7.listen(8000,()=>{console.log("Port:8000");})
+function serverSeven(req,res){
+    res.setHeader('Content-type', 'text/html');
+    res.end(`<h3>Welcome to Altcampus</h3>`);
 }
 
-function handleServerEight(req, res) {
-  res.writeHead(200, {
-    'Content-type': 'text/html',
-  });
-  res.end(`<h3>Welcome to altcampus using write head</h3>`);
+
+server8=http.createServer(serverEight)
+server8.listen(8888,()=>{console.log("Port:8888");})
+function serverEight(req,res){
+    res.writeHead(200, {
+        'Content-type': 'application/json',
+      });
+      res.end(`{ success: true, message: 'Welcome to Nodejs' }`);
 }
 
-function handleServerNine(req, res) {
-  res.writeHead(200, {
-    'Content-type': 'application/json',
-  });
-  res.end(`{ success: true, message: 'Welcome to Nodejs' }`);
+
+server9=http.createServer(serverNine)
+server9.listen(5050,()=>{console.log("Port:5050");})
+function serverNine(req,res){
+    path=res.url
+    if(res.method=="POST"&&path=='/index'){
+        res.setHeader('Content-type', 'text/html');
+        res.end(`<h2>posted for first time</h2>`)
+    }
 }
 
-serverOne.listen(5000, () => {
-  console.log('Server One is Up');
-});
 
-serverTwo.listen(5100, () => {
-  console.log('Server Two is Up');
-});
 
-serverThree.listen(5555, () => {
-  console.log('Server Three is up');
-});
 
-serverFour.listen(5566, () => {
-  console.log('Server four is up');
-});
+server10=http.createServer(serverTen)
+server10.listen(2345,()=>{console.log("Port:2345");})
+function serverTen(req,res){
+    if(res.method=="GET"&&path=='/'){
+        res.setHeader('Content-type', 'text/plain');
+        res.end(`<h2>posted for first time</h2>`)
+    }
+    else if(res.method=="GET"&&path=='/about'){
+        res.setHeader('Content-type', 'text/html');
+        res.end(`<h2>posted for first time</h2>`)
+    }
+}
 
-serverFive.listen(7000, () => {
-  console.log('Server 5 listening on port 7000');
-});
 
-serverSix.listen(3333, () => {
-  console.log('Server 6 is up');
-});
+server11=http.createServer(serverEleven)
+server11.listen(3000,()=>{console.log("Port:3000");})
+function serverEleven(req,res){
+    res.end()//?
+}
 
-serverSeven.listen(8000, () => {
-  console.log('server seven is up');
-});
 
-serverEight.listen(8001, () => {
-  console.log('Server eight is up');
-});
+server12=http.createServer(serverTwelve)
+server12.listen(4000,()=>{console.log("Port:4000");})
+function serverTwelve(req,res){
+    res.end()//?
+}
 
-serverNine.listen(8888, () => {
-  console.log('Server nine is up');
-});
+
